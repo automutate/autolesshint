59597e2b4ba4b44bc5b713cbd91e557fb6165f72
Elorfin <penin.axel@gmail.com>
2017-02-23 09:50:23 +0100

    fixes import of autoMutator

--- src/runner.ts
+++ src/runner.ts
@@ -1,4 +1,4 @@
-import { AutoMutator } from "automutate/lib/automutator";
+import { AutoMutator } from "automutate/lib/autoMutator";
 import * as fs from "fs";
 
 import { fileContentsGetter } from "./fileContentsGetter";
